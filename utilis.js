0fc1e6226de3abc91f44ebd37a17a956f958d220
pawelalbanowski <pawel.albanowski@gmail.com>
2020-07-28 13:54:37 +0200

    prettier

--- utilis.js
+++ utilis.js
@@ -1,9 +1,12 @@
 const Methods = {
-  splitByNewLine: text => {
+  splitByNewLine: (text) => {
     return text.split("\r\n");
   },
-  countOdd: numbers =>  {
-      return numbers.reduce((count, number) => count+ (Number(number)%2==0 ? 0 : 1), 0);
-  }
-}
+  countOdd: (numbers) => {
+    return numbers.reduce(
+      (count, number) => count + (Number(number) % 2 == 0 ? 0 : 1),
+      0
+    );
+  },
+};
 module.exports = Methods;
--- version1.js
+++ version1.js
@@ -1,8 +1,8 @@
-const methods = require('./utilis.js');
+const methods = require("./utilis.js");
 
-var fs = require('fs');
+var fs = require("fs");
 
-fs.readFile('./numbers.txt', 'utf-8', (err, data) => {
+fs.readFile("./numbers.txt", "utf-8", (err, data) => {
   if (err) throw err;
   data = methods.splitByNewLine(data);
   console.log(data);
