47c94674ae0313721d1d9ed424263d1c2c5a8e65
pawelalbanowski <pawel.albanowski@gmail.com>
2020-07-29 11:20:47 +0200

    bonus

---
+++ version4.js
@@ -0,0 +1,19 @@
+const methods = require("./utilis.js");
+
+const fs = require("fs");
+const fsPromises = fs.promises;
+
+
+async function showAmountOdds() {
+  try {
+    const content = await fsPromises.readFile("./numbers.txt", "utf-8");
+    console.log(content.toString());
+    const lines = methods.splitByNewLine(content);
+    const numOdds = methods.countOdd(lines);
+    console.log(numOdds);
+  } catch (err) {
+    throw err;
+  }
+}
+
+showAmountOdds();
