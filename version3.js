3f99040b33e210f949797ad75652a48d574747f2
pawelalbanowski <pawel.albanowski@gmail.com>
2020-07-29 10:42:40 +0200

    Create version3.js

---
+++ version3.js
@@ -0,0 +1,21 @@
+const methods = require("./utilis.js");
+
+const fs = require("fs");
+
+const readFileAsArray = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf-8", (err, data) => {
+      if (err) reject(err);
+      const dataArray = methods.splitByNewLine(data);
+      console.log(dataArray);
+      resolve(dataArray);
+    });
+  });
+};
+
+async function showAmountOdds() {
+  const lines = await readFileAsArray("./numbers.txt");
+  console.log(methods.countOdd(lines));
+}
+
+showAmountOdds();
